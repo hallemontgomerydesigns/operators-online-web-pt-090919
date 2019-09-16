8bdcc9f79c714f2ccbc3417b1fc5468e6874d35e
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-16 17:19:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,15 @@
 def unsafe?(speed)
-  
-  if speed > 60
+  if speed > 60 
     true
   elsif speed < 40
-  true 
-  
-else 
-  false
-  
+    true
+  else
+    false
+  end
 end
 
-
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? true : false
+  speed < 40 || speed > 60 ? true : false
 end
 	
 
